97edd8a342c6dc2cff81247b132574b0f146f344
olleikali <ali_olleik@hotmail.com>
2025-01-22 03:52:09 +0100

    terraform aws

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 # Create vpc
 resource "aws_vpc" "v1" {
   cidr_block = "172.120.0.0/16"
+  #cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_hostnames = true
   enable_dns_support = true
